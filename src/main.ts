b271a882ee5eaf9ccf689a7ca3f088676fca363f
Evelyn Gordon <evelyn.gordon20@gmail.com>
2022-05-31 00:55:51 -0500

    Set resoltion to window.devicePixelDensity automatically

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,12 @@ import { LoadingScreen } from "./screens/loading";
 import { SoundLoader } from "@pixi/sound";
 
 Loader.registerPlugin(SoundLoader);
-(window as any).PIXI = PIXI
+(window as any).PIXI = PIXI;
 
-const app = new Application();
+const app = new Application({
+  autoDensity: true,
+  resolution: window.devicePixelRatio,
+});
 const container = document.getElementById("container") as HTMLElement;
 app.resizeTo = container;
 container.appendChild(app.view);
@@ -25,7 +28,9 @@ const screenManager = new ScreenManager();
 
 // screenManager.loadScreen(() => new LoadingScreen(app, screenManager, 1011011, 2116202)); // new beginnings
 // screenManager.loadScreen(() => new LoadingScreen(app, screenManager, 1020313, 2134868)); // Triangles (No elements, one .osu)
-screenManager.loadScreen(() => new LoadingScreen(app, screenManager, 606998, 1315750)); // Stars Align (Simple)
+screenManager.loadScreen(
+  () => new LoadingScreen(app, screenManager, 606998, 1315750)
+); // Stars Align (Simple)
 // screenManager.loadScreen(() => new LoadingScreen(app, screenManager, 11122, 1315750)); // Night Flight [Breeze] (no IDs in MetaData), unique bg per difficulty
 // screenManager.loadScreen(() => new LoadingScreen(app, screenManager, 1416017, 2921425)); // After School (mp4 video)
 // screenManager.loadScreen(() => new LoadingScreen(app, screenManager, 36858, 120893)); // Senbonzakura (flv video)
