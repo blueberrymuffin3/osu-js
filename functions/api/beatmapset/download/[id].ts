56d8ff53035b9d0a80819d97c4683874a94555df
Evelyn Gordon <evelyn.gordon20@gmail.com>
2022-06-08 22:26:24 +0000

    Improve error handling of worker

--- functions/api/beatmapset/download/[id].ts
+++ functions/api/beatmapset/download/[id].ts
@@ -1,4 +1,16 @@
-export const onRequestGet: PagesFunction = ({ params }) => {
-  return fetch(`https://beatconnect.io/b/${params.id}/`);
-  // return fetch(`https://kitsu.moe/api/d/${params.id}`); // No Video
+export const onRequestGet: PagesFunction = async ({ params }) => {
+  let id: number = Number(params.id as string);
+
+  const res = await fetch(`https://beatconnect.io/b/${id}/`);
+  if (res.status == 200) {
+    return new Response(res.body);
+  } else if (res.status == 404) {
+    return new Response("No beatmapset found", {
+      status: 404,
+    });
+  } else {
+    return new Response(`Error fetching beatmap: got code ${res.status}`, {
+      status: 500,
+    });
+  }
 };
