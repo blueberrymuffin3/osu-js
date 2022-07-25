7d292f79d8a729d90043ad5762a55b616ea9557d
Evelyn Gordon <evelyn.gordon20@gmail.com>
2022-07-25 19:39:51 +0000

    Remove unused sound file from preload

--- src/play/resources/sounds.ts
+++ src/play/resources/sounds.ts
@@ -1,8 +1,8 @@
-import SOUND_INTRO_SEEYA from "./osu/osu.Game.Resources/Samples/Intro/seeya.mp3";
+// import SOUND_INTRO_SEEYA from "./osu/osu.Game.Resources/Samples/Intro/seeya.mp3";
 
-import { sound } from "@pixi/sound";
-sound.add(Object.fromEntries([SOUND_INTRO_SEEYA].map((url) => [url, url])));
+// import { sound } from "@pixi/sound";
+// sound.add(Object.fromEntries([SOUND_INTRO_SEEYA].map((url) => [url, url])));
 
-export { SOUND_INTRO_SEEYA };
+// export { SOUND_INTRO_SEEYA };
 
-export const preloadSounds = [SOUND_INTRO_SEEYA];
+export const preloadSounds = [];
