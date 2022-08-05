59be1dab23c11da903bb99cf43f7bd455324f037
Evelyn Gordon <evelyn.gordon20@gmail.com>
2022-08-05 00:39:19 +0000

    Try fetching beatmapsets from multiple origins. 3rd party mirrors often go down or return strange error messages, sometimes even with a 200 status code.

--- functions/api/download/beatmapset/[id].ts
+++ functions/api/download/beatmapset/[id].ts
@@ -1,11 +1,27 @@
 import { cachedApiResponseHelper } from "../../../lib";
 
-export const onRequestGet: PagesFunction = async ({ params, waitUntil }) =>
-  cachedApiResponseHelper(
-    // `https://beatconnect.io/b/${Number(params.id)}/`,
-    `https://kitsu.moe/api/d/${Number(params.id)}`,
+const magicBytes = new Uint8Array([0x50, 0x4b, 0x03, 0x04]);
+
+export const onRequestGet: PagesFunction = async ({ params, waitUntil }) => {
+  const id = Number(params.id);
+
+  if (isNaN(id) || id <= 0) {
+    return new Response("Bad Request", { status: 400 });
+  }
+
+  return cachedApiResponseHelper(
+    [
+      `https://kitsu.moe/api/d/${id}`,
+      `https://beatconnect.io/b/${id}/`,
+      `https://chimu.moe/d/${id}`,
+      `https://catboy.best/d/${id}`,
+      `https://proxy.nerinyan.moe/d/${id}`,
+    ],
     {
       waitUntil,
       ttl: 3600,
+      cacheKey: `cache:download-beatmapset/${id}`,
+      magicBytes,
     }
   );
+};
