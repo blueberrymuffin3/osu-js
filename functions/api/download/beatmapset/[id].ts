d88bb206e4217f9f861cbe3cd6ff7d8417d4ea20
Evelyn Gordon <evelyn.gordon20@gmail.com>
2022-08-05 19:40:47 +0000

    Return beatmapsets directly from chimu.moe, uncached. Caching was adding significant latency to the requests, and kitsu.moe doesn't return videos outside of the US. Fixes #39

--- functions/api/download/beatmapset/[id].ts
+++ functions/api/download/beatmapset/[id].ts
@@ -1,11 +1,20 @@
-import { cachedApiResponseHelper } from "../../../lib";
+export const onRequestGet: PagesFunction = async ({ params }) => {
+  const id = Number(params.id);
 
-export const onRequestGet: PagesFunction = async ({ params, waitUntil }) =>
-  cachedApiResponseHelper(
-    // `https://beatconnect.io/b/${Number(params.id)}/`,
-    `https://kitsu.moe/api/d/${Number(params.id)}`,
-    {
-      waitUntil,
-      ttl: 3600,
-    }
-  );
+  if (isNaN(id) || Math.floor(id) !== id || id <= 0) {
+    return new Response("Bad Request", { status: 400 });
+  }
+  const res = await fetch(`https://chimu.moe/d/${id}`);
+  if (res.status === 200) {
+    return new Response(res.body, {
+      headers: {
+        "content-length": res.headers.get("content-length"),
+        "content-type": "application/x-osu-archive",
+      },
+    });
+  } else if (res.status == 404) {
+    return new Response("Not Found", { status: 404 });
+  } else {
+    return new Response(`Got code ${res.status} from origin`, { status: 500 });
+  }
+};
