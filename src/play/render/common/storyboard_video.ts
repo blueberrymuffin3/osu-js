276ec6a80aacb77bbf7dff1cebf97e3c64dcf24a
kionell <sashakostyunin1@gmail.com>
2022-08-06 09:20:10 +0300

    Reorganize stuff in storyboard video renderer

--- src/play/render/common/storyboard_video.ts
+++ src/play/render/common/storyboard_video.ts
@@ -12,53 +12,60 @@ const MAX_VIDEO_SKEW_SEEK = 0.5;
 export class DrawableStoryboardVideo 
   extends DrawableStoryboardElement<StoryboardVideo> 
 {
-  private static video: HTMLVideoElement | null = null;
-
-  private video: HTMLVideoElement;
+  private background: Texture | null = null;
+  private video: HTMLVideoElement | null = null;
   private videoStartTime: number | null = null;
   private videoStarted = false;
-
-  private backgroundTexture: Texture | null = null;
   
   constructor(object: StoryboardVideo, beatmap: LoadedBeatmap) {
     super(object);
 
-    // Use a single HTML video element for all storyboard videos.
-    if (!DrawableStoryboardVideo.video) {
-      DrawableStoryboardVideo.video = document.createElement("video");
-    }
-    
-    this.video = DrawableStoryboardVideo.video;
-    this.backgroundTexture = beatmap.background ?? null;
-    
+    this.background = beatmap.background ?? null;
+
     const videoUrl = beatmap.videoURLs.get(object.filePath)
     
-    if (!videoUrl) return;
+    if (!videoUrl) {
+      this.switchToBackground();
+      return;
+    };
 
-    this.video.src = videoUrl;
-    this.video.muted = true;
-    this.video.autoplay = false;
-    this.video.addEventListener(
-      "error",
-      (error) => {
-        console.error("Error playing video", error.error || error);
-        this.video.removeAttribute('src');
-        
-        if (this.backgroundTexture) {
-          this.texture = this.backgroundTexture;
-        }
-      },
-      true
-    );
-
+    this.video = document.createElement("video");
     this.videoStartTime = object.startTime / 1000;
 
     if (this.videoStartTime < 0) {
       this.video.currentTime = -this.videoStartTime;
     }
+
+    this.video.muted = true;
+    this.video.autoplay = false;
+
+    this.video.addEventListener(
+      "error",
+      (error) => {
+        console.error("Error playing video", error.error || error);
+        this.switchToBackground();
+      },
+      true
+    );
+    
+    this.video.src = videoUrl;
   }
 
   update(timeMs: number): void {
+    adaptiveScaleDisplayObject(
+      VIRTUAL_SCREEN,
+      this.texture,
+      this,
+      POLICY.FullWidth
+    );
+
+    if (this.video === null) return;
+
+    // Current video is ended.
+    if (this.video.ended) {
+      return this.switchToBackground();
+    }
+
     const timeElapsed = timeMs / 1000;
     
     // Video hasn't been started yet.
@@ -68,43 +75,40 @@ export class DrawableStoryboardVideo
     
     const targetVideoTime = timeElapsed - this.videoStartTime!;
 
-    // Current video is ended.
-    if (targetVideoTime > this.video.duration) {
-      this.video.removeAttribute('src');
-      if (this.backgroundTexture) {
-        this.texture = this.backgroundTexture;
-      }
-    }
-
-    // Do this only once at the video start
+    // Do this only once at the start of the video
     if (!this.videoStarted) {
       this.texture = Texture.from(this.video);
       this.video.play();
       this.videoStarted = true;
     }
 
-    const skew = this.video!.currentTime - targetVideoTime;
+    const skew = this.video.currentTime - targetVideoTime;
     
     if (Math.abs(skew) > MAX_VIDEO_SKEW_SEEK) {
-      this.video!.currentTime = targetVideoTime;
-      this.video!.playbackRate = 1;
-      console.warn("Video skew high, seeking");
-    } else if (Math.abs(skew) > MAX_VIDEO_SKEW_SPEED) {
-      console.warn("Video skew high, changing playbackRate");
-      if (skew > 0) {
-        this.video!.playbackRate = 0.5;
-      } else {
-        this.video!.playbackRate = 2;
-      }
-    } else {
-      this.video!.playbackRate = 1;
+      this.video.currentTime = targetVideoTime;
+      this.video.playbackRate = 1;
+      // console.warn("Video skew high, seeking");
+      return;
+    }
+    
+    if (Math.abs(skew) > MAX_VIDEO_SKEW_SPEED) {
+      this.video.playbackRate = skew > 0 ? 0.5 : 2;
+      // console.warn("Video skew high, changing playbackRate");
+      return;
+    }
+    
+    this.video.playbackRate = 1;
+  }
+
+  switchToBackground(): void {
+    if (this.video !== null) {
+      this.video.pause();
+      this.video.removeAttribute('src');
+      this.video = null;
     }
 
-    adaptiveScaleDisplayObject(
-      VIRTUAL_SCREEN,
-      this.texture,
-      this,
-      POLICY.FullWidth
-    );
+    if (this.background !== null) {
+      this.texture = this.background;
+    }
   }
 }
