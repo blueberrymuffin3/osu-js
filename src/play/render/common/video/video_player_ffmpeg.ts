1874e6aaa40c88b4a2e4b5077462e24b5e1e901b
Evelyn Gordon <evelyn.gordon20@gmail.com>
2022-08-08 02:40:00 +0000

    Properly copy video who's width isn't divisible by 12. Fixes #46

--- src/play/render/common/video/video_player_ffmpeg.ts
+++ src/play/render/common/video/video_player_ffmpeg.ts
@@ -20,7 +20,13 @@ declare global {
   } | null;
 }
 
+const BYTES_PER_PIXEL = 3;
+const LINE_ALIGNMENT = 4;
+
 export class VideoPlayerFFmpeg extends Sprite implements IUpdatable {
+  // null means no alignment is needed
+  private lineSizeBytes: number | null = null;
+
   constructor(videoUrl: string) {
     super();
 
@@ -45,10 +51,17 @@ export class VideoPlayerFFmpeg extends Sprite implements IUpdatable {
   update(timeMs: number): void {
     if (window.PIXI_STREAM && this.texture === Texture.EMPTY) {
       if (this.texture === Texture.EMPTY) {
+        let lineSizeBytes = window.PIXI_STREAM.width * BYTES_PER_PIXEL;
+
+        if (lineSizeBytes % LINE_ALIGNMENT !== 0) {
+          lineSizeBytes =
+            lineSizeBytes + LINE_ALIGNMENT - (lineSizeBytes % LINE_ALIGNMENT);
+          // Manual alignment is needed
+          this.lineSizeBytes = lineSizeBytes;
+        }
+
         this.texture = Texture.fromBuffer(
-          new Uint8Array(
-            window.PIXI_STREAM.width * window.PIXI_STREAM.height * 4
-          ),
+          new Uint8Array(lineSizeBytes * window.PIXI_STREAM.height),
           window.PIXI_STREAM.width,
           window.PIXI_STREAM.height,
           {
@@ -66,7 +79,24 @@ export class VideoPlayerFFmpeg extends Sprite implements IUpdatable {
         const timeMsRounded = Math.round(timeMs);
         if (timeMsRounded >= window.PIXI_STREAM.ptsMs!) {
           let resource = this.texture.baseTexture.resource as BufferResource;
-          resource.data.set(window.PIXI_STREAM.data, 0);
+
+          if (this.lineSizeBytes === null) {
+            // Pixel data is already properly aligned
+            resource.data.set(window.PIXI_STREAM.data, 0);
+          } else {
+            // Pixel data must be aligned manually
+            const lineSizeFF = window.PIXI_STREAM.width * BYTES_PER_PIXEL;
+            for (let line = 0; line < window.PIXI_STREAM.height; line++) {
+              resource.data.set(
+                window.PIXI_STREAM.data.subarray(
+                  lineSizeFF * line,
+                  lineSizeFF * (line + 1)
+                ),
+                line * this.lineSizeBytes
+              );
+            }
+          }
+
           this.texture.update();
 
           Atomics.store(window.PIXI_STREAM.timeMs, 0, timeMsRounded);
