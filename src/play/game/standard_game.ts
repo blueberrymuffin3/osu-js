b821449a8f7776884d00b9ee36cc1c7c25ed5c27
Evelyn Gordon <evelyn.gordon20@gmail.com>
2022-08-13 03:53:22 +0000

    Fix frame time statistics not being logged after the audio ended

--- src/play/game/standard_game.ts
+++ src/play/game/standard_game.ts
@@ -99,6 +99,32 @@ export class StandardGame extends Container {
     app.ticker.add(this.tick, this);
 
     this.audio.play();
+    this.audio.on("end", () => {
+      this.frameTimes ??= [];
+      console.log("Rendered", this.frameTimes.length, "frames");
+      this.frameTimes.sort((a, b) => a - b);
+      const Ps = [50, 90, 99, 99.9, 99.99];
+      for (const P of Ps) {
+        console.log(
+          `P${P}`,
+          this.frameTimes[
+            Math.floor((this.frameTimes.length * P) / 100)
+          ].toFixed(2)
+        );
+      }
+      console.log("min", this.frameTimes[0].toFixed(2));
+      console.log(
+        "max",
+        this.frameTimes[this.frameTimes.length - 1].toFixed(2)
+      );
+      console.log(
+        "mean",
+        (
+          this.frameTimes.reduce((a, b) => a + b) / this.frameTimes.length
+        ).toFixed(2)
+      );
+      this.frameTimes = null;
+    });
   }
 
   protected tick() {
@@ -127,32 +153,6 @@ export class StandardGame extends Container {
     }
 
     this.frameTimes?.push(this.app.ticker.elapsedMS);
-
-    if (this.frameTimes && this.timeElapsedMs >= this.audioDurationMs) {
-      console.log("Rendered", this.frameTimes.length, "frames");
-      this.frameTimes.sort((a, b) => a - b);
-      const Ps = [50, 90, 99, 99.9, 99.99];
-      for (const P of Ps) {
-        console.log(
-          `P${P}`,
-          this.frameTimes[
-            Math.floor((this.frameTimes.length * P) / 100)
-          ].toFixed(2)
-        );
-      }
-      console.log("min", this.frameTimes[0].toFixed(2));
-      console.log(
-        "max",
-        this.frameTimes[this.frameTimes.length - 1].toFixed(2)
-      );
-      console.log(
-        "mean",
-        (
-          this.frameTimes.reduce((a, b) => a + b) / this.frameTimes.length
-        ).toFixed(2)
-      );
-      this.frameTimes = null;
-    }
 
     this.hitObjectTimeline.update(this.timeElapsedMs);
     this.cursorAutoplay.update(this.timeElapsedMs);
