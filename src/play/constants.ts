b14d87ad8cbd407faee1c46aae5beeeb9901d92b
kionell <sashakostyunin1@gmail.com>
2022-08-15 15:18:38 +0300

    Add constant to detect if device is using iOS

--- src/play/constants.ts
+++ src/play/constants.ts
@@ -141,5 +141,20 @@ export const getAllFramePaths = (element: StoryboardAnimation) => {
   return paths;
 };
 
+export const isUsingIOS = (function () {
+  var iosQuirkPresent = function () {
+      var audio = new Audio();
+
+      audio.volume = 0.5;
+      return audio.volume === 1;   // volume cannot be changed from "1" on iOS 12 and below
+  };
+
+  var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+  var isAppleDevice = navigator.userAgent.includes('Macintosh');
+  var isTouchScreen = navigator.maxTouchPoints >= 1;   // true for iOS 13 (and hopefully beyond)
+
+  return isIOS || (isAppleDevice && (isTouchScreen || iosQuirkPresent()));
+})();
+
 export const isUsingFirefox = navigator.userAgent.includes("Firefox");
 export const firefoxMaxTimeBetweenUpdates = 0.1; // Time should update about once every 40ms
