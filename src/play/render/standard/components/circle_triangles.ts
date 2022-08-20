1e885798ffe94a4c30e9ba96c794bfc7e3176ce9
kionell <sashakostyunin1@gmail.com>
2022-08-21 02:21:36 +0300

    Randomize alpha of the circle triangles

--- src/play/render/standard/components/circle_triangles.ts
+++ src/play/render/standard/components/circle_triangles.ts
@@ -9,6 +9,7 @@ import {
 import { TEXTURE_SKIN_DEFAULT_GAMEPLAY_OSU_DISC } from "../../../resources/textures";
 import { IUpdatable } from "../../../game/timeline";
 import { Triangles } from "./triangles";
+import { MathUtils } from "osu-classes";
 
 const _MASK = new Graphics();
 _MASK.beginFill(0xffffff);
@@ -16,6 +17,9 @@ _MASK.drawCircle(0, 0, 128);
 _MASK.endFill();
 
 const MASK = _MASK.geometry;
+
+const MIN_TRIANGLE_ALPHA = 0.18;
+const MAX_TRIANGLE_ALPHA = 0.23;
 
 export class CircleTriangles extends Sprite implements IUpdatable {
   private trianglesGeometry: Triangles;
@@ -38,7 +42,11 @@ export class CircleTriangles extends Sprite implements IUpdatable {
     this.trianglesMesh.y = -this.height * 0.5;
     this.trianglesMesh.scale.x = this.width;
     this.trianglesMesh.scale.y = this.height;
-    this.trianglesMesh.alpha = 0.3;
+    this.trianglesMesh.alpha = MathUtils.clamp(
+      Math.random(), 
+      MIN_TRIANGLE_ALPHA, 
+      MAX_TRIANGLE_ALPHA
+    );
     this.trianglesMesh.tint = color;
     this.trianglesMesh.blendMode = BLEND_MODES.ADD;
     this.trianglesMesh.mask = this.trianglesMask;
